2a52a1b147d9992807f8d6ef23d6fa675e90552a
ebn huz <ebnhgs@gmail.com>
2020-12-24 15:52:51 -0500

    Add Delete Function

--- BookListRazor/Controllers/BookController.cs
+++ BookListRazor/Controllers/BookController.cs
@@ -22,24 +22,24 @@ namespace BookListRazor.Controllers
 
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Book.ToList() });
+            return Json(new { data = await _db.Book.ToListAsync() });
         }
 
-        //[HttpDelete]
-        //public async Task<IActionResult> Delete(int id)
-        //{
-        //    var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
-        //    if (bookFromDb == null)
-        //    {
-        //        return Json(new { success = false, message = "Error while Deleting" });
-        //    }
-        //    _db.Book.Remove(bookFromDb);
-        //    await _db.SaveChangesAsync();
-        //    return Json(new { success=true, message = "Delete Successful" });
-        
-        //}
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
 
     }
 
